704bfc8f0fe376aedf85a0e29f969f6773b33065
Luu Kim Loan <luukimloan1991@gmail.com>
2014-11-18 11:12:26 +0700

    Them module Hang Hoa

---
+++ module/HangHoa/config/module.config.php
@@ -0,0 +1,64 @@
+<?php
+return array(
+	'controllers' => array(
+		'invokables' => array(
+			'HangHoa\Controller\Index' => 'HangHoa\Controller\IndexController',            
+		),
+	),
+    'router' => array(
+        'routes' => array(
+            'hang_hoa' => array(
+                'type'    => 'literal', 
+                'options' => array(
+                    'route'    => '/hang-hoa',                     
+                    'defaults' => array(
+                       '__NAMESPACE__'=>'HangHoa\Controller',
+                        'controller' => 'Index',
+                        'action'     => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(                    
+                    'crud' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '[/][:action][/:id]',
+                            'constraints' => array(                            
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id'=>'[0-9]+',
+                            ),                            
+                        ),
+                    ),                
+                ),
+             ),
+         ),
+     ),    
+
+	'view_manager' => array(
+		'template_path_stack' => array(
+			'hang_hoa' => __DIR__ . '/../view'
+		),
+        'template_map'=>array(
+            'layout/giaodien'        => __DIR__ . '/../view/layout/giao-dien.phtml',
+        ),
+	),    
+
+	/*'doctrine' => array(
+        'driver' => array(
+            'hang_hoa_annotation_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__.'/../src/HangHoa/Entity',//Edit
+                ),
+            ),
+
+            'orm_default' => array(
+                'drivers' => array(
+
+                    'HangHoa\Entity' => 'hang_hoa_annotation_driver'//Edit
+                )
+            )
+        )
+    ),*/
+);
---
+++ module/HangHoa/src/HangHoa/Controller/IndexController.php
@@ -0,0 +1,42 @@
+<?php namespace HangHoa\Controller;
+
+ use Zend\Mvc\Controller\AbstractActionController;
+ use Zend\View\Model\ViewModel;
+ use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator as DoctrineAdapter;
+ use Zend\ServiceManager\ServiceManager;
+ 
+ class IndexController extends AbstractActionController
+ {
+ 	private $entityManager;
+
+  public function getEntityManager()
+  {
+     if(!$this->entityManager)
+     {
+      //$this->entityManager=$this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+     }
+     return $this->entityManager;
+  }
+  
+ 	public function indexAction()
+ 	{
+    $this->layout('layout/giaodien');
+ 	}
+
+  public function uploadFormAction()
+  {        
+  }
+
+ 	public function addAction()
+ 	{
+ 	}
+
+ 	public function editAction()
+ 	{   
+ 	}
+
+ 	public function deleteAction()
+ 	{        
+  }
+ }
+?>
