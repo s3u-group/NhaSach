c3698c380a42dea1101313f5e26cb6c7ce618f93
Luu Kim Loan <luukimloan1991@gmail.com>
2014-11-19 14:09:55 +0700

    Hoan than co ban 4 module

---
+++ module/LoiNhuan/config/module.config.php
@@ -0,0 +1,61 @@
+<?php
+return array(
+	'controllers' => array(
+		'invokables' => array(
+			'LoiNhuan\Controller\Index' => 'LoiNhuan\Controller\IndexController',            
+		),
+	),
+    'router' => array(
+        'routes' => array(
+            'loi_nhuan' => array(
+                'type'    => 'literal', 
+                'options' => array(
+                    'route'    => '/loi-nhuan',
+                    'defaults' => array(
+                       '__NAMESPACE__'=>'LoiNhuan\Controller',
+                        'controller' => 'Index',
+                        'action'     => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(                    
+                    'crud' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '[/][:action][/:id]',
+                            'constraints' => array(                            
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id'=>'[0-9]+',
+                            ),                            
+                        ),
+                    ),            
+                ),
+             ),
+         ),
+     ),    
+
+	'view_manager' => array(
+		'template_path_stack' => array(
+			'loi_nhuan' => __DIR__ . '/../view'
+		),        
+	),    
+
+	/*'doctrine' => array(
+        'driver' => array(
+            'hang_hoa_annotation_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__.'/../src/HangHoa/Entity',//Edit
+                ),
+            ),
+
+            'orm_default' => array(
+                'drivers' => array(
+
+                    'HangHoa\Entity' => 'hang_hoa_annotation_driver'//Edit
+                )
+            )
+        )
+    ),*/
+);
