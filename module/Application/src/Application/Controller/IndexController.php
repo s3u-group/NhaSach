1bc938372ef182974ca94d4de50776971204d9c6
phanvanthanh <phanvanthanhda10tt@gmail.com>
2014-11-19 14:20:08 +0700

    index application

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -16,6 +16,7 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+    	$this->layout('layout/giaodien');
         return new ViewModel();
     }
 }
