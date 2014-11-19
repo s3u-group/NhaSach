ccac78ede89893f3705ed1aedc076ab20a690861
Luu Kim Loan <luukimloan1991@gmail.com>
2014-11-19 16:10:22 +0700

    Them Form

--- module/HangHoa/src/HangHoa/Controller/IndexController.php
+++ module/HangHoa/src/HangHoa/Controller/IndexController.php
@@ -4,6 +4,14 @@
  use Zend\View\Model\ViewModel;
  use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator as DoctrineAdapter;
  use Zend\ServiceManager\ServiceManager;
+ use HangHoa\Entity\SanPham;
+ use HangHoa\Form\CreateBangTinForm;
+
+ use Zend\Validator\File\Size;
+
+ use Zend\Stdlib\AbstractOptions;
+ 
+ use S3UTaxonomy\Form\CreateTermTaxonomyForm;
  
  class IndexController extends AbstractActionController
  {
@@ -13,7 +21,7 @@
   {
      if(!$this->entityManager)
      {
-      //$this->entityManager=$this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+      $this->entityManager=$this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
      }
      return $this->entityManager;
   }
@@ -45,7 +53,23 @@
 
   public function themSanPhamAction()
   {
-    $this->layout('layout/giaodien');  
+    $this->layout('layout/giaodien'); 
+
+    $entityManager=$this->getEntityManager();
+    $sanPham=new sanPham();
+    $form= new CreateSanPhamForm($entityManager);
+    $form->bind($bangTin);
+
+    $taxonomyLoai=$this->TaxonomyFunction();
+    $loais=$taxonomyLoai->getListChildTaxonomy('danh-muc-hang-hoa');// đưa vào taxonomy dạng slug
+    
+    $taxonomyDonViTinh=$this->TaxonomyFunction();
+    $donViTinhs=$taxonomyDonViTinh->getListChildTaxonomy('don-vi-tinh');// đưa vào taxonomy dạng slug
+    return array(
+      'form' => $form, 
+      'loais'=>$loais,
+      'donViTinhs'=>$donViTinhs,      
+    ); 
   }
 
  	public function addAction()
---
+++ module/HangHoa/src/HangHoa/Form/CreateSanPhamForm.php
@@ -0,0 +1,39 @@
+<?php
+namespace HangHoa\Form;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
+use Zend\Form\Form;
+use HangHoa\Form\SanPhamFieldset;
+
+use Zend\InputFilter\InputFilterProviderInterface;
+use Zend\Form\Element;
+use Zend\InputFilter\InputFilter;
+
+
+class CreateSanPhamForm extends Form
+{
+    public function __construct(ObjectManager $objectManager)
+    {
+        parent::__construct('create-san-pham');
+        
+        // The form will hydrate an object of type "BlogPost"
+        $this->setHydrator(new DoctrineHydrator($objectManager));
+
+        // Add the user fieldset, and set it as the base fieldset
+        $sanPhamFieldset = new SanPhamFieldset($objectManager);
+        $sanPhamFieldset->setUseAsBaseFieldset(true);
+        $this->add($sanPhamFieldset);        
+
+        $this->add(array(
+             'name' => 'submit',
+             'type' => 'Submit',
+             'attributes' => array(
+                 'value' => 'Go',
+                 'id' => 'submitbutton',
+             ),
+         ));        
+    }
+        
+}
+?>
