bdf900cfadb4ebdaa11a37dcb1f63f6858d52283
Luu Kim Loan <luukimloan1991@gmail.com>
2014-11-24 16:58:57 +0700

    Dang lam phan phpd excel

--- module/HangHoa/src/HangHoa/Controller/IndexController.php
+++ module/HangHoa/src/HangHoa/Controller/IndexController.php
@@ -19,7 +19,7 @@
  use Zend\Stdlib\AbstractOptions;
  
  use S3UTaxonomy\Form\CreateTermTaxonomyForm;
-
+ use PHPExcel;
 
  
  class IndexController extends AbstractActionController
@@ -332,8 +332,6 @@
   public function searchSanPhamAction()
   {
     $response=array();
-
-
     $request=$this->getRequest();
     if($request->isXmlHttpRequest())
     {
@@ -359,7 +357,20 @@
     }
     $json = new JsonModel($response);
     return $json;
-
   }
+
+  public function importAction()
+  {
+    $this->layout('layout/giaodien');
+
+    $objPHPExcel = new PHPExcel();
+    $objPHPExcel->setActiveSheetIndex(0)->mergeCells('A1:D1');
+    $objPHPExcel->getActiveSheet()->setCellValue('A1', 'Loan');
+    //$objWriter =  new phpexcel \ Classes\ PHPExcel\ IOFactory;
+    //$objWriter::createWriter($objPHPExcel, 'Excel2007');
+    //$filename = "luukimloan".".xlsx";
+    //$objWriter->save($filename);
+  }
+  
  }
 ?>
