01d57ba62d497db0b5fd1438e335b6ef8359f081
phanvanthanh <phanvanthanhda10tt@gmail.com>
2014-11-24 11:23:27 +0700

    xong phan kenh phan phoi

---
+++ module/KenhPhanPhoi/src/KenhPhanPhoi/View/Helper/GetSoHoaDonVaHoaDonMoiNhat.php
@@ -0,0 +1,48 @@
+<?php
+namespace KenhPhanPhoi\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+
+class  GetSoHoaDonVaHoaDonMoiNhat extends AbstractHelper{
+
+	private $entityManager; 
+    
+	public function getEntityManager()
+    {       
+        if(!$this->entityManager)
+         {
+          $this->entityManager=$this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+         }
+         return $this->entityManager;
+    }
+	
+	public function setEntityManager($entityManager)
+	{
+		$this->entityManager=$entityManager;
+	}
+
+	
+	public function __invoke($idDoiTac){
+        $array=array();
+        $entityManager=$this->getEntityManager();
+
+        $repository = $entityManager->getRepository('HangHoa\Entity\HoaDon');
+        $queryBuilder = $repository->createQueryBuilder('hd');
+        $queryBuilder->add('where','hd.idDoiTac='.$idDoiTac.' ORDER BY hd.ngayXuat DESC');
+        $query = $queryBuilder->getQuery();
+        $hoaDons = $query->execute();
+       
+       
+        $soHoaDon=count($hoaDons);
+        $hoaDonMoiNhat=$hoaDons[0]->getMaHoaDon();
+        $idHoaDon=$hoaDons[0]->getIdHoaDon();
+        $array=array(
+            'soHoaDon'=>$soHoaDon,
+            'hoaDonMoiNhat'=>$hoaDonMoiNhat,
+            'idHoaDon'=>$idHoaDon,
+        );
+        return $array;
+		
+	}
+}
+?>
