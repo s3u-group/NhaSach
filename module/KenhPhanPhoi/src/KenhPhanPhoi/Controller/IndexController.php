080209887ca1b3c1a5851044c9243c6eda54437a
phanvanthanh <phanvanthanhda10tt@gmail.com>
2014-11-24 15:16:33 +0700

    xong phan kenhphanphoi

--- module/KenhPhanPhoi/src/KenhPhanPhoi/Controller/IndexController.php
+++ module/KenhPhanPhoi/src/KenhPhanPhoi/Controller/IndexController.php
@@ -58,7 +58,8 @@
            return $this->redirect()->toRoute('kenh_phan_phoi/crud');
       }  
       $entityManager=$this->getEntityManager();
-      //$hoaDon=$entityManager->getRepository()->find($id);
+      $hoaDon=$entityManager->getRepository('HangHoa\Entity\HoaDon')->find($id);
+      die(var_dump($hoaDon));
   }
 
  	public function themKhachHangAction()
