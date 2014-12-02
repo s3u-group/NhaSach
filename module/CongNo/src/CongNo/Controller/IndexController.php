083b37714be5c793921f7dc4313a94e6bc0d4b3f
phanvanthanh <phanvanthanhda10tt@gmail.com>
2014-12-02 13:57:59 +0700

    dang lam phan congno

--- module/CongNo/src/CongNo/Controller/IndexController.php
+++ module/CongNo/src/CongNo/Controller/IndexController.php
@@ -94,7 +94,7 @@ namespace CongNo\Controller;
       if($tenKhachHang)
       {
         $entityManager=$this->getEntityManager();
-        $query = $entityManager->createQuery('SELECT kh FROM HangHoa\Entity\DoiTac kh WHERE kh.hoTen LIKE :ten');
+        $query = $entityManager->createQuery('SELECT kh FROM HangHoa\Entity\DoiTac kh WHERE kh.loaiDoiTac=45 and kh.hoTen LIKE :ten');
         $query->setParameter('ten','%'.$tenKhachHang.'%');// % đặt ở dưới này thì được đặt ở trên bị lỗi
         $khachHangs = $query->getResult(); // array of CmsArticle objects 
         foreach ($khachHangs as $khachHang) {
@@ -103,6 +103,67 @@ namespace CongNo\Controller;
             'tenKhachHang'=>$khachHang->getHoTen(),            
           );
         }
+      }
+    }
+
+    $json = new JsonModel($response);
+    return $json;
+  }
+
+  public function searchCongNoKhachHangAction()
+  {
+    $response=array();
+
+    $request=$this->getRequest();
+    if($request->isXmlHttpRequest())
+    {
+      $data=$request->getPost();
+      $idDoiTac=$data['idDoiTac'];
+      if($idDoiTac)
+      {
+        $entityManager=$this->getEntityManager();
+        $query = $entityManager->createQuery('SELECT pt FROM HangHoa\Entity\DoiTac kh, CongNo\Entity\CongNo cn, CongNo\Entity\PhieuThu pt  WHERE kh.idDoiTac=cn.idDoiTac and cn.idCongNo=pt.idCongNo and kh.idDoiTac= :idDoiTac ORDER BY pt.ngayThanhToan DESC, pt.idPhieuThu DESC');
+        $query->setParameter('idDoiTac',$idDoiTac);// % đặt ở dưới này thì được đặt ở trên bị lỗi
+        $congNos = $query->getResult(); // array of CmsArticle objects 
+
+        // nếu đã có công nợ trước với hệ thống
+        if($congNos)
+        {
+          $ngayDauKi=$congNos[0]->getNgayThanhToan()->format('Y-m-d');
+          $noDauKi=$congNos[0]->getIdCongNo()->getDuNo();
+          
+        }
+        else// khách hàng mới tạo chưa có công nợ với hệ thống lần nào
+        {
+          // nợ đầu kỳ
+          $noDauKi=0;
+          $dT=$entityManager->getRepository('HangHoa\Entity\DoiTac')->find($idDoiTac);
+
+          // lấy ngày đăng ký làm ngày đầu kỳ
+          $ngayDauKi=$dT->getNgayDangKy()->format('Y-m-d');
+        }
+
+        // lấy nợ phát sinh hoaDon
+        $query=$entityManager->createQuery('SELECT hd FROM HangHoa\Entity\hoaDon hd WHERE hd.status=0 and hd.idDoiTac= :idDoiTac');
+        $query->setParameter('idDoiTac',$idDoiTac);// % đặt ở dưới này thì được đặt ở trên bị lỗi
+        $hoaDons=$query->getResult();
+
+        
+        $noPhatSinh=0;
+        foreach ($hoaDons as $hoaDon) {
+          foreach ($hoaDon->getCtHoaDons() as $ctHoaDon) {
+            $noPhatSinh+=(float)$ctHoaDon->getGia()*(float)$ctHoaDon->getSoLuong();
+          }
+        }
+
+        // tính nợ cuối kỳ
+        $noCuoiKi=(float)$noDauKi+(float)$noPhatSinh;
+        $response=array(
+          'ngayDauKi'=>$ngayDauKi,
+          'noDauKi'=>$noDauKi,
+          'noPhatSinh'=>$noPhatSinh,
+          'noCuoiKi'=>$noCuoiKi,
+        );
       }
     }
 
